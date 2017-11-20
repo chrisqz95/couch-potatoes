379bad30e5aabd33da080090fd2330b55763843a
Casey Schneider <clschnei@ucsd.edu>
2017-11-19 19:36:25 -0800

    Added documentation and refined a bit

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
@@ -26,8 +26,9 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
 
-import java.io.Serializable;
-
+/**
+ * The home page which shows potential matches.
+ */
 public class MainActivity extends AppCompatActivity {
     private DBHelper helper;
     private android.widget.Button logout;
@@ -38,28 +39,32 @@ public class MainActivity extends AppCompatActivity {
     private android.widget.Button friendsBtn;
     private android.widget.ImageButton messengerBtn;
 
+    // For the user cards
     private SwipePlaceHolderView mSwipeView;
     private Context mContext;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         helper = new DBHelper();
 
+        // Find the id's of the buttons
         profileBtn = (android.widget.ImageButton) findViewById(R.id.profileBtn);
         datingBtn = (android.widget.Button) findViewById(R.id.datingBtn);
         friendsBtn = (android.widget.Button) findViewById(R.id.friendsBtn);
         messengerBtn = (android.widget.ImageButton) findViewById(R.id.messengerBtn);
-
         acceptBtn = (android.widget.ImageButton) findViewById(R.id.acceptBtn);
         rejectBtn = (android.widget.ImageButton) findViewById(R.id.rejectBtn);
         logout = findViewById(R.id.logout);
 
+        // Set up how many cards are displayed as a stack
         mSwipeView = (SwipePlaceHolderView) findViewById(R.id.swipeView);
         mContext = getApplicationContext();
         mSwipeView.getBuilder().setDisplayViewCount(3).setSwipeDecor(new SwipeDecor()
                 .setPaddingTop(20).setRelativeScale(0.01f));
 
+        // Adds fake users for testing purposes
         User user_test1 = new MatchedUser(null, null, "Bob", null, "Smith",
                 null, null, "Los Angleles", null, null, null,
                 0, 0, false, false);
@@ -81,13 +86,13 @@ public class MainActivity extends AppCompatActivity {
         mSwipeView.addView(new UserCard(mContext, user_test4, mSwipeView));
         mSwipeView.addView(new UserCard(mContext, user_test5, mSwipeView));
 
+        // Simulate a swipe right or left by pressing the bottom buttons
         acceptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mSwipeView.doSwipe(true);
             }
         });
-
         rejectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Log out and display the log in screen
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -105,6 +111,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Grabs a user's info and displays it in a swipe-able card.
+     *
+     *
+     */
     @Layout(R.layout.card_view)
     public class UserCard {
         @com.mindorks.placeholderview.annotations.View(R.id.profileImageView)
@@ -121,12 +132,22 @@ public class MainActivity extends AppCompatActivity {
         private SwipePlaceHolderView mSwipeView;
         private String image;
 
+        /**
+         * Create a card which displays a user's info.
+         *
+         * @param context - current activity
+         * @param user - the user to display info for
+         * @param swipeView - space used to detect a swipe
+         */
         public UserCard(Context context, User user, SwipePlaceHolderView swipeView) {
             mContext = context;
             mUser = user;
             mSwipeView = swipeView;
         }
 
+        /**
+         * When the card is clicked, open a page displaying more user info.
+         */
         @Click(R.id.cardView)
         private void onClick() {
             Intent intent = new Intent(getApplicationContext(), UserActivity.class);
@@ -134,6 +155,9 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        /**
+         * Populate the image and text fields of the card.
+         */
         @Resolve
         private void onResolved() {
             image = "http://www.aft.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png";
@@ -142,27 +166,42 @@ public class MainActivity extends AppCompatActivity {
             locationNameTxt.setText(mUser.getCity());
         }
 
+        /**
+         * When the card is swiped left, log the event
+         */
         @SwipeOut
         private void onSwipedOut() {
             Log.d("EVENT", "onSwipedOut");
             mSwipeView.addView(this);
         }
 
+        /**
+         * When the card is released but wasn't swiped, reset to its original position
+         */
         @SwipeCancelState
         private void onSwipeCancelState() {
             Log.d("EVENT", "onSwipeCancelState");
         }
 
+        /**
+         * When the card is swiped right, log the event
+         */
         @SwipeIn
         private void onSwipeIn() {
             Log.d("EVENT", "onSwipedIn");
         }
 
+        /**
+         * Detects when the card is moved enough to the right to count as an accept
+         */
         @SwipeInState
         private void onSwipeInState() {
             Log.d("EVENT", "onSwipeInState");
         }
 
+        /**
+         * Detects when the card is moved enough to the left to count as a reject
+         */
         @SwipeOutState
         private void onSwipeOutState() {
             Log.d("EVENT", "onSwipeOutState");
--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/UserActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/UserActivity.java
@@ -3,7 +3,10 @@ package com.example.potato.couchpotatoes;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -12,39 +15,51 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 /**
- * Created by casey on 11/18/17.
+ * A page that displays more info about a potential match
  */
-
 public class UserActivity extends AppCompatActivity {
     private ImageView profileImageView;
     private TextView nameAgeTxt;
     private TextView locationNameTxt;
-    private ImageButton backBtn;
     private User mUser;
     private Context mContext;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // Creates a back button to return to the main page
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Finds the image and text field id's
         profileImageView = (ImageView) findViewById(R.id.profileImageView);
         nameAgeTxt = (TextView) findViewById(R.id.nameAgeTxt);
         locationNameTxt = (TextView) findViewById(R.id.locationNameTxt);
-        backBtn = (ImageButton) findViewById(R.id.backBtn);
 
+        // Gets the user info that was passed from the main page
         mUser = (User) getIntent().getSerializableExtra("UserInfo");
         mContext = getApplicationContext();
 
+        // Populates the image and text fields
         String image = "http://www.aft.com/components/com_easyblog/themes/wireframe/images/placeholder-image.png";
         Glide.with(mContext).load(image).into(profileImageView);
         nameAgeTxt.setText(mUser.getFirstName());
         locationNameTxt.setText(mUser.getCity());
+    }
 
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+    /**
+     * When the back button is pressed, return to the main page.
+     *
+     * @param item - the back button
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.home)
+            this.finish();
+
+        return super.onOptionsItemSelected(item);
     }
 }
