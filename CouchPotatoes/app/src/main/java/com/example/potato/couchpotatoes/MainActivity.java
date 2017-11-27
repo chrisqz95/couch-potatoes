93475145ed1cc3cb7ec1fa66a3531b287aa97c18
Mervin Ng <m6ng@ucsd.edu>
2017-11-26 22:48:49 -0800

    added much code into skeleton but still needs work; need to grab user info/object from Firebase; need to implement Parcelable object

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.potato.couchpotatoes;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private DBHelper helper;
@@ -14,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private android.widget.Button chat;
 
     private PullUserInfoTask mPullTask = null;
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mMainActivityView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +43,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Display user's name if logged in
         if ( helper.isUserLoggedIn() ) {
-            // String displayName = helper.getAuthUserDisplayName();
 
-            // userName.setText( displayName );
-
+            // TODO: complete code in the method attemptPullUserInfo()
+            // TODO: DELETE CODE BELOW once above todo is completed
+            // goes to MatchingActivity.class
             startActivity(new Intent(getApplicationContext(), MatchingActivity.class));
             finish();
+
+            // User is logged in, load up the user object with all the logged user's info!
+            // This will also render MatchActivity.class upon success
+            attemptPullUserInfo();
+
         }
         // Else, redirect user to login page
         else {
@@ -62,10 +80,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity( intent );
             }
         });
+
+        mMainActivityView = findViewById(R.id.main_activity_screen);
+        mProgressView = findViewById(R.id.main_activity_progressbar);
+    }
+
+
+    /**
+     * Shows the progress UI and hides the Main Activity layout until user object is loaded
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mMainActivityView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mMainActivityView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mMainActivityView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mMainActivityView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 
     /**
-     * Attempt to pull the user's info
+     * Attempt to pull the user's info from Firebase. Once the user info is retrieved inside a user
+     * object, it will be passed into a bundle so we can pass that object into MatchingActivity
      */
     private void attemptPullUserInfo() {
         if (mPullTask == null) {
@@ -73,42 +132,85 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // TODO if there is any logic that needs to go before starting the task is put here
+        // ^comment: this logic work could possibly be done through onPreExecute on the AsyncTask
 
-        mPullTask = new PullUserInfoTask();
+        // TODO: retrieve the correct authorized user object or the correct info to fill in a user object
+        // fake user being passed down to Matching Activity
+        // (for testing purposes only)
+        CurrentUser user_test1 = new CurrentUser(null, null, "Robin",
+                "McLaurin", "Williams", null, null, "Chicago",
+                "Illinois", "USA", "Seize the day!", 0, 0,
+                false, false);
+
+        // while system loads up the user object and pass it to MatchingActivity,
+        // system shall display a progress bar on screen
+        showProgress(true);
+
+
+        mPullTask = new PullUserInfoTask(user_test1);
         // TODO:  we can pass in anything into the method below and will appear in doInBackground.
         // see documentation for more details
         mPullTask.execute((Void) null);
     }
 
 
-    // If we need to pass some sort of information to the asyncTast, replace the first
+    // If we need to pass some sort of information to the asyncTask, replace the first
     // Void with the object type we need
     public class PullUserInfoTask extends AsyncTask<Void, Void, Boolean> {
 
-        // TODO use firebase to fill in this information
-        private CurrentUser user;
-
-
+        // TODO use Firebase to fill the user object in this information
+        private CurrentUser mUser;
+        // ^problem: FirebaseUser is NOT the same object as CurrentUser or User class
 
         // TODO pass in any needed information here
-        PullUserInfoTask() {}
+        PullUserInfoTask(CurrentUser user) {
+            mUser = user;
+        }
 
         // alternatively, we can pass in information during execute in which we will get the info here
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: on success do this
+
+            // TODO: implement myParcelable class that extends the "Parcelable" interface
+            // How to send user object to another activity via the Parcelable interface
+            // https://stackoverflow.com/questions/2139134/how-to-send-an-object-from-one-android-activity-to-another-using-intents
+            // MyParcelable mParcelableUser;
+
+            // unrenders the progress bar screen
+            showProgress(false);
+
+            // TODO: on success, insert user object into a bundle and put it inside the intent!
             // We will need to change User object to implement Parcelable so that we can pass both Current user and
             // MatchedUser to different activites/fragments
-//        Intent intent = new Intent(getApplicationContext(), MatchingActivity.class);
-//        intent.putExtra("UserObject", user);
-//        startActivity(intent);
-            // TODO to retrieve it later, use
-            // CurrentUser user = getIntent().getParcelableExtra("UserObject");
+            Intent mIntent = new Intent(getApplicationContext(), MatchingActivity.class);
+            //mIntent.putExtra("UserObject", mUser); // stores User object in the Intent object
+            startActivity(mIntent);
+            finish();
+
+            // TODO:  to retrieve it later, use the line below!
+            //CurrentUser user = getIntent().getParcelableExtra("UserObject");
 
             return false;
         }
 
+
+
+
     }
+
+//  TODO: implement Parcelable class for passing User Object into Intent object for Matching Activity
+//    protected class MyParcelable extends Parcelable {
+//
+//        @Override
+//        public int describeContents() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public void writeToParcel(Parcel dest, int flags) {
+//
+//        }
+//    }
 
 }
 
