cab61a021c1c301c90a0035c9f3989edfefe8cf6
Andrew Huang <anh162@ucsd.edu>
2017-12-01 18:34:00 -0800

    Added delete functionality

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/PictureGridActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/PictureGridActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -17,10 +18,13 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.LruCache;
 import com.squareup.picasso.Picasso;
 
@@ -37,6 +41,7 @@ public class PictureGridActivity extends AppCompatActivity {
 //    private boolean uploadButton;
     private boolean isCurrentUser = false;
     private ArrayList<String> urlList;
+    private ArrayList<String> hashList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +63,18 @@ public class PictureGridActivity extends AppCompatActivity {
         dbHelper.fetchCurrentUser();
 
         urlList = new ArrayList<>();
+        hashList = new ArrayList<>();
 
-        final DatabaseReference listRef = dbHelper.getDb().getReference();
-        listRef.addValueEventListener(new ValueEventListener() {
+        final DatabaseReference ref = dbHelper.getDb().getReference();
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
+                urlList.clear();
+                hashList.clear();
                 for (DataSnapshot dataSnapshot : snapshot.child("User_Photo").child(uid).getChildren()) {
                     try {
                         urlList.add(snapshot.child("Photo").child(dataSnapshot.getKey()).child("uri").getValue().toString());
+                        hashList.add(dataSnapshot.getKey().toString());
                     } catch (NullPointerException e) {
                         e.printStackTrace();
                     }
@@ -89,12 +98,29 @@ public class PictureGridActivity extends AppCompatActivity {
                     gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
                         @Override
-                        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
                             new AlertDialog.Builder(PictureGridActivity.this)
                                     .setTitle("Are you sure you want to delete this image?")
                                     .setIcon(android.R.drawable.ic_dialog_alert)
                                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                         public void onClick(DialogInterface dialog, int whichButton) {
+                                            //StorageReference storageRef = dbHelper.getStorage().getReference(); //("Photo/pPqKDpd6TXaO5Utj7s3Te6OTaLT2/Tobedeleted.PNG");
+                                            //storageRef.child("Photo/").child(uid + "/").child(hashList.get(position));
+                                            StorageReference storageRef = dbHelper.getStorage().getReference("Photo/" + uid + "/" + hashList.get(position));
+                                            storageRef.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    final DatabaseReference dbRef = dbHelper.getDb().getReference();
+                                                    dbRef.child("Photo").child(hashList.get(position)).removeValue();
+                                                    dbRef.child("User_Photo").child(uid).child(hashList.get(position)).removeValue()
+                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                    loadData(uid, isCurrentUser);
+                                                                }
+                                                            });
+                                                }
+                                            });
                                         }
                                     })
                                     .setNegativeButton(android.R.string.no, null).show();
