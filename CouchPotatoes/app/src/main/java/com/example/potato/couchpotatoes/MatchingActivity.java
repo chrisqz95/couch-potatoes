8c09842cb413144b635d1d160a7e2fbd29a1d41d
Casey Schneider <clschnei@ucsd.edu>
2017-12-04 23:21:47 -0800

    Increased size of image

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MatchingActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MatchingActivity.java
@@ -16,6 +16,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
