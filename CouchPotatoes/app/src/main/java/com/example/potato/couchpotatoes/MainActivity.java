8616fcab5eec508e844fd60949b840ee3c40cda1
Mervin Ng <m6ng@ucsd.edu>
2017-12-09 13:34:29 -0800

    deleted unused imports and comments

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
@@ -1,52 +1,19 @@
 package com.example.potato.couchpotatoes;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.ActionBar;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
-import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
-import com.mindorks.placeholderview.annotations.Click;
-import com.mindorks.placeholderview.annotations.Layout;
-import com.mindorks.placeholderview.annotations.Resolve;
-import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
-import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
-import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
-import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
-import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
-
-import org.w3c.dom.Text;
-import java.lang.Double;
-import java.lang.Number.*;
 
 /**
  * The home page which shows potential matches.
@@ -69,10 +36,6 @@ public class MainActivity extends AppCompatActivity {
         // Sets up the activity layout
         super.onCreate(savedInstanceState);
 
-        //mMainActivityView = findViewById(R.id.main_activity_screen);
-        //mProgressView = findViewById(R.id.main_activity_progressbar);
-        //mProgressView.setVisibility();
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -92,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
             startActivity(new Intent(getApplicationContext(), MatchingActivity.class));
             finish();
-            //mProgressView.setVisibility(View.VISIBLE);
 
         }
         // Else, redirect user to login page
@@ -122,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for ( DataSnapshot field : dataSnapshot.getChildren() ) {
-                    // Log.d( "TEST", field.toString() );
                     switch ( field.getKey() ) {
                         case "email":
                             currentUser.setEmail( (String) field.getValue() );
