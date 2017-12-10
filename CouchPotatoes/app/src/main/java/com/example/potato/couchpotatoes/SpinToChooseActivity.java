f3b2afa688ee202f7134369de8130f901b0bc319
Sophia Krause-Levy <skrauselevy@gmail.com>
2017-12-09 16:19:05 -0800

    Spinners are all in one activity now.

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/SpinToChooseActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/SpinToChooseActivity.java
@@ -1,6 +1,5 @@
 package com.example.potato.couchpotatoes;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,30 +12,87 @@ import android.widget.Button;
 import android.widget.ImageView;
 import java.util.Random;
 
-// https://www.youtube.com/watch?v=TPKugVdd6E4
 public class SpinToChooseActivity  extends AppCompatActivity {
 
-    Button button;
-    ImageView ic_spinner;
-    ImageView ic_potato;
-    Random r;
-    int degree = 0, degree_old = 0;
-    int food;
-    // because there are 37 selectors 9.72 degrees each
-    private static final float FACTOR = 30f;
+    private Button button;
+    private ImageView ic_spinner;
+    private ImageView ic_potato;
+    private Random r;
+    private int degree = 0, degree_old = 0;
+    private int spinner_key;
+    private ImageView iv_bottle;
+
+    private String[] activityOptions = {"COUCHPOTATOES SUGGESTS:\n GO TO THE BEACH",
+            "COUCHPOTATOES SUGGESTS:\n GO TO A MOVIE",
+            "COUCHPOTATOES SUGGESTS: PLAY VIDEOGAMES",
+            "COUCHPOTATOES SUGGESTS:\n GO GET COFFEE",
+            "COUCHPOTATOES SUGGESTS:\n GO ON A HIKE",
+            "COUCHPOTATOES SUGGESTS:\n GO FOR A WALK"
+    };
+    private String[] foodOptions = {"COUCHPOTATOES SUGGESTS:\n PIZZA FOR DINNER",
+            "COUCHPOTATOES SUGGESTS:\n BURGERS FOR DINNER",
+            "COUCHPOTATOES SUGGESTS:\n ITALIAN FOOD FOR DINNER",
+            "COUCHPOTATOES SUGGESTS:\n MEXICAN FOOD FOR DINNER",
+            "COUCHPOTATOES SUGGESTS:\n JAPANESE FOOD FOR DINNER",
+            "COUCHPOTATOES SUGGESTS: CHINESE FOOD FOR DINNER"};
+    private String [] niceOptions = {"COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A HUG ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A HANDSHAKE ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A KISS ON THE CHEEK ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A PECK ON THE LIPS ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A WAVE ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A HIGH FIVE ;) " };
+    private String [] naughtyOptions = {"COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A MAKEOUT SESSION ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH AN ADVANCED HUG ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH SOME SEXUAL ACTIVITIES ;) ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A FRENCH KISS ;) ",
+            "COUCHPOTATOES SUGGESTS:\n GO HOME ",
+            "COUCHPOTATOES SUGGESTS:\n SAY GOODBYE WITH A SLAP ON THE BUTT ;)" };
+
+    private int foodImages [] = { R.drawable.potato_pizza,
+            R.drawable.potato_hamburger,
+            R.drawable.potato_italian,
+            R.drawable.potato_mexican,
+            R.drawable.potato_japanese,
+            R.drawable.potato_chinese
+    };
+
+    private int activityImages [] = { R.drawable.potato_pizza,
+            R.drawable.potato_movie,
+            R.drawable.potato_videogames,
+            R.drawable.potato_coffee,
+            R.drawable.potato_hike,
+            R.drawable.potato_walk
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spin_to_choose);
 
-        button = findViewById(R.id.button);
-        ic_spinner = findViewById(R.id.ic_spinner);
-        food = getIntent().getIntExtra("key", 0);
-        if(food == 0){
-            ic_spinner.setImageResource(R.mipmap.spinner_activity);
+        // Set correct view for chosen spinner
+        spinner_key = getIntent().getIntExtra("key", 0);
+        if(spinner_key == 0 || (spinner_key == 1)){
+            setContentView(R.layout.activity_spin_to_choose);
+            button = findViewById(R.id.button);
+            ic_spinner = findViewById(R.id.ic_spinner);
+            ic_potato = findViewById(R.id.ic_potato);
+
+            if(spinner_key == 0) {
+                ic_spinner.setImageResource(R.mipmap.spinner_food);
+            } else{
+                ic_spinner.setImageResource(R.mipmap.spinner_activity);
+            }
+        } else {
+            setContentView(R.layout.activity_spin_bottle);
+            iv_bottle = findViewById(R.id.iv_bottle);
+            button = findViewById(R.id.button);
+            ic_spinner = findViewById(R.id.ic_spinner);
+            if(spinner_key == 2){
+                ic_spinner.setImageResource(R.mipmap.spinner_nice);
+            } else{
+                ic_spinner.setImageResource(R.mipmap.spinner_naughty);
+            }
         }
-        ic_potato = findViewById(R.id.ic_potato);
+
         r = new Random();
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -51,12 +107,11 @@ public class SpinToChooseActivity  extends AppCompatActivity {
                 rotate.setInterpolator(new DecelerateInterpolator());
                 rotate.setAnimationListener(new Animation.AnimationListener() {
                     @Override
-                    public void onAnimationStart(Animation animation) {
-                    }
+                    public void onAnimationStart(Animation animation) {}
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
-                        String message = currentNumber(360 - (degree % 360), food);
+                        String message = currentNumber(360 - (degree % 360), spinner_key);
 
                         // return to main screen
                         final String message2 = message;
@@ -76,60 +131,74 @@ public class SpinToChooseActivity  extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onAnimationRepeat(Animation animation) {
-                        currentNumber(360 - (degree % 360), food);
-                    }
+                    public void onAnimationRepeat(Animation animation) {}
                 });
 
-                ic_spinner.startAnimation(rotate);
+                // Start appropriate spinner
+                if(spinner_key == 1 || spinner_key == 0){
+                    ic_spinner.startAnimation(rotate);
+                }
+                else {
+                    iv_bottle.startAnimation(rotate);
+                }
+
             }
         });
     }
 
-    private String currentNumber(int degrees, int food){
-        if(food == 1) {
-            switch ((degrees / (360 / 6)) % 6) {
-                case 0:
-                    ic_potato.setImageResource(R.drawable.potato_pizza);
-                    return "@strings/suggestion\n PIZZA FOR DINNER ";
-                case 1:
-                    ic_potato.setImageResource(R.drawable.potato_hamburger);
-                    return "COUCHPOTATOES SUGGESTS:\n BURGERS FOR DINNER ";
-                case 2:
-                    ic_potato.setImageResource(R.drawable.potato_italian);
-                    return "COUCHPOTATOES SUGGESTS:\n ITALIAN FOOD FOR DINNER ";
-                case 3:
-                    ic_potato.setImageResource(R.drawable.potato_mexican);
-                    return "COUCHPOTATOES SUGGESTS:\n MEXICAN FOOD FOR DINNER ";
-                case 4:
-                    ic_potato.setImageResource(R.drawable.potato_japanese);
-                    return "COUCHPOTATOES SUGGESTS:\n JAPANESE FOOD FOR DINNER ";
-                case 5:
-                    ic_potato.setImageResource(R.drawable.potato_chinese);
-                    return "COUCHPOTATOES SUGGESTS:\n CHINESE FOOD FOR DINNER ";
-            }
-        } else {
-            switch((degrees / (360 / 6)) % 6) {
-                case 0:
-                    ic_potato.setImageResource(R.drawable.potato_beach);
-                    return "COUCHPOTATOES SUGGESTS:\n GO TO THE BEACH ";
-                case 1:
-                    ic_potato.setImageResource(R.drawable.potato_movie);
-                    return "COUCHPOTATOES SUGGESTS:\n GO TO A MOVIE ";
-                case 2:
-                    ic_potato.setImageResource(R.drawable.potato_videogames);
-                    return "COUCHPOTATOES SUGGESTS:\n PLAY VIDEOGAMES ";
-                case 3:
-                    ic_potato.setImageResource(R.drawable.potato_coffee);
-                    return "COUCHPOTATOES SUGGESTS:\n GO GET COFFEE ";
-                case 4:
-                    ic_potato.setImageResource(R.drawable.potato_hike);
-                    return "COUCHPOTATOES SUGGESTS:\n GO ON A HIKE ";
-                case 5:
-                    ic_potato.setImageResource(R.drawable.potato_walk);
-                    return "COUCHPOTATOES SUGGESTS:\n GO FOR A WALK ";
-            }
+    private String currentNumber(int degrees, int spinner_key) {
+        int images[] = foodImages;
+        String options[] = foodOptions;
+        int chosen_image;
+        String chosen_option;
+
+        // grab correct spinner info
+        if(spinner_key == 1){
+            options = activityOptions;
+            images = activityImages;
+        } else if(spinner_key == 2){
+            options = niceOptions;
+        } else if(spinner_key == 3){
+            options = naughtyOptions;
         }
-        return "";
+
+        switch ((degrees / (360 / 6)) % 6) {
+            case 0:
+                chosen_image = images[0];
+                chosen_option = options[0];
+                break;
+            case 1:
+                chosen_image = images[1];
+                chosen_option =  options[1];
+                break;
+            case 2:
+                chosen_image = images[2];
+                chosen_option = options[2];
+                break;
+            case 3:
+                chosen_image = images[3];
+                chosen_option = options[3];
+                break;
+            case 4:
+                chosen_image = images[4];
+                chosen_option = options[4];
+                break;
+            case 5:
+                chosen_image = images[5];
+                chosen_option = options[5];
+                break;
+            default:
+                chosen_image = images[0];
+                chosen_option = "";
+                break;
+        }
+
+        // update image if needed
+        if((spinner_key == 0) || (spinner_key == 1)){
+            System.out.println(chosen_image);
+            ic_potato.setImageResource(chosen_image);
+        }
+
+        return chosen_option;
     }
 }
