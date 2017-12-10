475820079c26d8fe5e9d7df75f977e6b698bd4d5
Alan Mai <0110amai@gmail.com>
2017-12-09 18:30:53 -0800

    cleaned up some unused statements

--- CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
+++ CouchPotatoes/app/src/main/java/com/example/potato/couchpotatoes/MainActivity.java
@@ -1,35 +1,19 @@
 package com.example.potato.couchpotatoes;
 
-import android.app.ActionBar;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
-import com.mindorks.placeholderview.SwipePlaceHolderView;
 
 /**
  * The home page which shows potential matches.
  */
 public class MainActivity extends AppCompatActivity {
     private DBHelper helper;
-    private android.widget.TextView userName;
-    private android.widget.Button logout;
-    private FloatingActionButton acceptBtn;
-    private FloatingActionButton rejectBtn;
-
-    // For the user cards
-    private SwipePlaceHolderView mSwipeView;
-    private Context mContext;
-    private android.widget.Button chat;
-
-    private String currUserID;
 
     // AsyncTasks
     private FetchCurrentUserInfoTask mFetchCurrUserInfoTask;
@@ -38,31 +22,22 @@ public class MainActivity extends AppCompatActivity {
         // Sets up the activity layout
         super.onCreate(savedInstanceState);
 
-        //mMainActivityView = findViewById(R.id.main_activity_screen);
-        //mProgressView = findViewById(R.id.main_activity_progressbar);
-        //mProgressView.setVisibility();
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         helper = new DBHelper();
-        ActionBar actionBar = getActionBar();
 
-        userName = (android.widget.TextView) findViewById(R.id.userName);
-        chat = (android.widget.Button) findViewById(R.id.viewChats);
-        currUserID = helper.getAuth().getUid();
+        android.widget.Button chat = (android.widget.Button) findViewById(R.id.viewChats);
 
         // Display user's name if logged in
         if ( helper.isUserLoggedIn() ) {
 
             // reads all the user info on the current user
-//            pullCurrentUserInfo();
             attemptFetchCurUserInfo();
 
             startActivity(new Intent(getApplicationContext(), MatchingActivity.class));
             finish();
-            //mProgressView.setVisibility(View.VISIBLE);
 
         }
         // Else, redirect user to login page
@@ -76,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( getApplicationContext(), ChatRoomActivity.class );
-                //intent.putExtra( "userName", userName.getText() );
                 startActivity( intent );
             }
         });
